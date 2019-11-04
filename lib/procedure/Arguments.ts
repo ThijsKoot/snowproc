b73a09f49d5139f25a8c8b659e389d6123d20f09
Thijs Koot <thijs.koot@kadenza.nl>
2019-11-04 09:17:22 +0100

    added jsdocs to argument

--- lib/procedure/Arguments.ts
+++ lib/procedure/Arguments.ts
@@ -1,3 +1,13 @@
+/**
+ * @abstract Base class for declaring procedure arguments.
+ * Values will be assigned by the compiler.
+ * @example 
+ * export class AssignRoleArguments extends Arguments {
+ *     stringArg: string;
+ *     dateArg: Date;
+ *     numberArg: number;
+ * }
+ */
 export abstract class Arguments {
 
 }
