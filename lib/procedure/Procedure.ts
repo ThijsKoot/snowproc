f281da21a8a9814185c2c4f8795b0884410dc536
Thijs Koot <thijs.koot@kadenza.nl>
2019-11-04 09:17:09 +0100

    removed obsolete returnValue from Procedure

--- lib/procedure/Procedure.ts
+++ lib/procedure/Procedure.ts
@@ -2,9 +2,7 @@ import { Arguments } from "./Arguments";
 import { Rights } from "./Rights";
 
 export abstract class Procedure {
-
     name: string;
-    returnValue: any;
     canReturnNull: boolean = true;
     args: Arguments;
     rights: Rights = Rights.Owner;
